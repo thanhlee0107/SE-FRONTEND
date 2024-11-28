daf84e32c32ca495eefefe8503700817b7b41375
duydngn89 <duy.nguyend8998@hcmut.edu.vn>
2024-11-29 01:01:49 +0700

    gen 100 printer

--- be/src/printer_management/dbPrinter.js
+++ be/src/printer_management/dbPrinter.js
@@ -1,16 +1,17 @@
 const db = require("../../config/db");
 
 const createPrinterTable = `CREATE TABLE IF NOT EXISTS Printer(
-    ID INT AUTO_INCREMENT NOT NULL UNIQUE,
-    spsoId INT,
-    Model VARCHAR(20),
-    Brand VARCHAR(20),
-    Status ENUM('online', 'offline', 'maintaining') DEFAULT 'online',
-    Campus CHAR(3),
-    Building VARCHAR(3),
-    Floor INT,
-    Description VARCHAR(200),
-    PRIMARY KEY (ID)
+  ID INT AUTO_INCREMENT NOT NULL UNIQUE,
+  spsoId INT,
+  Model VARCHAR(20),
+  Brand VARCHAR(20),
+  Status ENUM('online', 'offline', 'maintaining') DEFAULT 'online',
+  Campus CHAR(3),
+  Building VARCHAR(3),
+  Floor INT,
+  Description VARCHAR(200),
+  printWaiting INT DEFAULT 0,
+  PRIMARY KEY (ID)
 );
 `;
 
@@ -27,29 +28,85 @@ const createPermittedFileTable = `CREATE TABLE IF NOT EXISTS PermittedFile (
 );`;
 
 db.query(createConfigTable, (err) => {
-    if (err) {
-        console.log("Error creating Config table:", err);
-    } else {
-        console.log("Config table created successfully");
-    }
+  if (err) {
+    console.log("Error creating Config table:", err);
+  } else {
+    console.log("Config table created successfully");
+  }
 });
 
 db.query(createPermittedFileTable, (err) => {
-    if (err) {
-        console.log("Error creating File table:", err);
-    } else {    
-        console.log("File type table created successfully");
-    }
+  if (err) {
+    console.log("Error creating File table:", err);
+  } else {
+    console.log("File type table created successfully");
+  }
 });
+const campusBuilding = {
+  CS1: ["B1", "B3", "B2", "B4", "B6", "B9", "A4", "A1", "C5", "C6"],
+  CS2: ["H1", "H2", "H3", "H6"],
+};
 
-const insertPrinterData = [
-  `INSERT INTO Printer (spsoId, Model, Brand, Status, Campus, Building, Floor, Description) VALUES 
-    (1, 'Printer 1', 'HP', 'online', 'CS1', 'B1', 4, 'Printer for 4th floor'),
-    (5, 'Printer 2', 'Canon', 'offline', 'CS2', 'H2', 5, 'Printer for 5th floor'),
-    (1, 'Printer 3', 'Epson', 'online', 'CS1', 'B3', 6, 'Printer for 6th floor'),
-    (1, 'Printer 3', 'Epson', 'online', 'CS2', 'H3', 2, 'Printer for 2th floor'),
-    (1, 'Printer 4', 'Epson', 'maintaining', 'CS1', 'B3', 6, 'Printer for 6th floor')`,
+const brands = [
+  "HP",
+  "Canon",
+  "Epson",
+  "Brother",
+  "Samsung",
+  "Lexmark",
+  "Ricoh",
 ];
+const statuses = ["online", "offline", "maintaining"];
+const descriptions = [
+  "Printer for 1st floor",
+  "Printer for 2nd floor",
+  "Printer for 3rd floor",
+  "Printer for 4th floor",
+  "Printer for 5th floor",
+  "Printer for 6th floor",
+  "Printer for 7th floor",
+  "Printer for 8th floor",
+];
+const floors = [1, 2, 3, 4, 5, 6, 7, 8];
+const getStatus = () => {
+  const random = Math.random() * 100; // Generate a random number between 0 and 100
+
+  if (random < 70) {
+    return "online"; 
+  } else if (random < 90) {
+    return "offline"; 
+  } else {
+    return "maintaining";
+  }
+};
+const generatePrinterData = () => {
+  const insertStatements = [];
+
+  let printerId = 1; // Start from 21 as 20 are already generated.
+  for (let i = 0; i < 100; i++) {
+    const campus = Object.keys(campusBuilding)[Math.floor(Math.random() * 2)];
+    const building =
+      campusBuilding[campus][
+        Math.floor(Math.random() * campusBuilding[campus].length)
+      ];
+    const brand = brands[Math.floor(Math.random() * brands.length)];
+    const status =  getStatus();
+    const floor = floors[Math.floor(Math.random() * floors.length)];
+    const description = descriptions[floor - 1];
+    const printWaiting = Math.floor(Math.random() * 500) + 1;
+
+    insertStatements.push(
+      `(1, 'Printer ${printerId}', '${brand}', '${status}', '${campus}', '${building}', ${floor}, '${description}', ${printWaiting})`
+    );
+    printerId++;
+  }
+
+  return `INSERT INTO Printer (spsoId, Model, Brand, Status, Campus, Building, Floor, Description, printWaiting) VALUES \n${insertStatements.join(
+    ",\n"
+  )};`;
+};
+
+const insertPrinterData = [generatePrinterData()];
 
 const restartPrinterDatabase = async () => {
   db.query(`DROP TABLE IF EXISTS PrintStatus`, (err) => {
